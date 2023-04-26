e2ab189f5603be0a9b02480aff3f86698c2b8466
nirdar1992 <131364971+nirdar1992@users.noreply.github.com>
2023-04-26 10:04:56 +0300

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,13 @@
+import json
+
+
 def extract_family_name(drill_name):
+    '''
+    :param drill_name: drill's name as a string
+    :return: family name
+
+    will be replaced by "map_drill_fam" function after orin's family grouping
+    '''
     try:
         first_word = drill_name[:drill_name.index(" ")]
     except:
@@ -9,3 +18,32 @@ def extract_family_name(drill_name):
         return "INNER GAMES"
     else:
         return first_word
+
+
+def map_drill_fam(drill):
+    '''
+    finds drill's family group
+    :param drill: drill's name
+    :return: drill's family name
+    '''
+    # load families json file
+    families_file = "data/families.json"
+    with open(families_file) as js_file:
+        families = json.load(js_file)
+    # iterate over each family until found
+    for fam in families:
+        if drill in families[fam]:
+            return fam
+    # if not found - raise an Error
+    raise ValueError("Drill was not found in the families dict.\n please check you've spelled it right.")
+
+
+def remove_num_of_parameter_from_drill_name(drill):
+    '''
+    cleans csv add-ons to the column's name
+    :param drill: drill's name
+    :return: drill's name without the number of occurrence
+    '''
+    # find the '.' before the number of the occurrence and return the original name
+    dot_index = drill.index('.')
+    return drill[:dot_index]
