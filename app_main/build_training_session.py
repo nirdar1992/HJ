59f23dddbbae6a5be707d60db038efd5859085ae
nirdar1992 <131364971+nirdar1992@users.noreply.github.com>
2023-09-09 11:23:47 +0300

    Add files via upload

--- app_main/build_training_session.py
+++ app_main/build_training_session.py
@@ -1,3 +1,4 @@
+import pprint
 from app_main.helper_functions import *
 from app_main.project_classes import SessionBuild
 from scipy.stats import norm
@@ -7,7 +8,7 @@ SD_MEAN = 0.2
 
 
 def rec_sum(families, drills, val, fam_name, parameter, sd, sub_drills=[]):
-    '''
+    """
     helper recursive function which finds a drill/s or drills combinations that sums up to a specific drill
     :param drills: list of family related drills names
     :param val: the target value we want to reach
@@ -16,7 +17,7 @@ def rec_sum(families, drills, val, fam_name, parameter, sd, sub_drills=[]):
     :param sd: val param * SD_MEAN global definition
     :param sub_drills: list of sub drills that were already found
     :return: list of lists of drills combinations that could replace the target drill
-    '''
+    """
     # if the sub drills are in the right value range - return it
     if sd >= val >= (sd * (-1)):
         return sub_drills
@@ -26,7 +27,9 @@ def rec_sum(families, drills, val, fam_name, parameter, sd, sub_drills=[]):
     else:
         # subtract the new drill parameter value from our target value
         find_parameter(parameter, families[fam_name][drills[0]])
-        temp_val = val - families[fam_name][drills[0]]["parameters"][parameter]["team's mean"]
+        temp_val = (
+            val - families[fam_name][drills[0]]["parameters"][parameter]["team's mean"]
+        )
         # remove the drill from our family drills because we've just iterated over it
         temp_drills = [drill for drill in drills]
         # get that drill name
@@ -35,40 +38,54 @@ def rec_sum(families, drills, val, fam_name, parameter, sd, sub_drills=[]):
         new_sbd = [drill for drill in sub_drills]
         new_sbd.append(d)
         # call the function recursively, once without taking the last drill in consider and once with it
-        return rec_sum(families, temp_drills, val, fam_name, parameter, sd, sub_drills),\
-               rec_sum(families, temp_drills, temp_val, fam_name, parameter, sd, new_sbd)
+        return rec_sum(
+            families, temp_drills, val, fam_name, parameter, sd, sub_drills
+        ), rec_sum(families, temp_drills, temp_val, fam_name, parameter, sd, new_sbd)
 
 
-def find_equal_drill(drill_name, sd, families_file="../data/families.json", parameter="Total Distance"):
-    '''
+def find_equal_drill(
+    drill_name, sd, families_file="../data/families.json", parameter="Total Distance"
+):
+    """
     function which finds a drill/s or drills combinations that sums up to a specific drill parameter value
     :param families_file: frills families file path
     :param drill_name: drill's name we would like to replace
     :param sd: accuracy range
     :param parameter: parameter's name we search by
     :return: list of lists of drills combinations that could replace the target drill
-    '''
+    """
     families = open_json_file(families_file)
     # find drill's family name
     fam_name = extract_family_name(drill_name)
     # extract the target value
     find_parameter(parameter, families[fam_name][drill_name])
-    mean_to_reach = families[fam_name][drill_name]["parameters"][parameter]["team's mean"]
+    mean_to_reach = families[fam_name][drill_name]["parameters"][parameter][
+        "team's mean"
+    ]
     # create list of the drills names from the same family and remove the drill we want to replace
     fam_drills = list(families[fam_name].keys())
     fam_drills.remove(drill_name)
     # call the "rec_sum" function and find our drills, print/return it
-    result = rec_sum(families, fam_drills, mean_to_reach, fam_name, parameter, mean_to_reach*sd)
+    result = rec_sum(
+        families, fam_drills, mean_to_reach, fam_name, parameter, mean_to_reach * sd
+    )
+    print("len", len(result))
+    print("type", type(result))
+    for i in result:
+        print(i)
+        print("\n\n\n\n")
     return result
 
 
-def get_session_val(drills, families_file="../data/families.json", parameter="Total Distance"):
-    '''
+def get_session_val(
+    drills, families_file="../data/families.json", parameter="Total Distance"
+):
+    """
     :param families_file: frills families file path
     :param drills: unlimited drills names that represents a training session
     :param parameter: by which parameter to sum the session. Distance param by default.
     :return: dictionary which contains the training session's parameter by drill and the total sum of it
-    '''
+    """
     families = open_json_file(families_file)
     total_dist = 0
     session_dict = {parameter: 0, "session's drills": {}}
@@ -77,23 +94,26 @@ def get_session_val(drills, families_file="../data/families.json", parameter="To
         # find drill's family name
         drill_fam = extract_family_name(drill)
         # copy drill's parameter values to the training session dict
-        session_dict["session's drills"][drill] = families[drill_fam][drill]["parameters"]
+        session_dict["session's drills"][drill] = families[drill_fam][drill][
+            "parameters"
+        ]
         # update the total training session "Distance per Minute (alt.)" parameter
         find_parameter(parameter, families[drill_fam][drill])
         total_dist += families[drill_fam][drill]["parameters"][parameter]["team's mean"]
     session_dict[parameter] = total_dist
+    pprint.pprint(session_dict)
     return session_dict
 
 
 def build_drills_combination(families, fam1, fam2, parameter):
-    '''
+    """
     helper function to the "build_session" function - find all unite drills combinations by families
     :param families: dictionary of all family drills
     :param fam1: family name as a string
     :param fam2: family name as a string OR list of drills combinations
     :param parameter: the parameter to sum
     :return: list of all drills combinations between two or more families
-    '''
+    """
     combinations = []
     # get family 1 data from the families dict
     fam1_dict = families.get(fam1)
@@ -106,10 +126,14 @@ def build_drills_combination(families, fam1, fam2, parameter):
             find_parameter(parameter, fam1_dict[drill1])
             for drill2 in fam2_dict:
                 find_parameter(parameter, fam2_dict[drill2])
-                drill1_val = fam1_dict[drill1]['parameters'][parameter]["team's mean"]
-                drill2_val = fam2_dict[drill2]['parameters'][parameter]["team's mean"]
+                drill1_val = fam1_dict[drill1]["parameters"][parameter]["team's mean"]
+                drill2_val = fam2_dict[drill2]["parameters"][parameter]["team's mean"]
                 # save to list as a list - [(drill from fam1, it's distance value), (drill from fam2, it's distance value), there total distance sum]
-                temp_combo = [(drill1, drill1_val), (drill2, drill2_val), drill1_val + drill2_val]
+                temp_combo = [
+                    (drill1, drill1_val),
+                    (drill2, drill2_val),
+                    drill1_val + drill2_val,
+                ]
                 combinations.append(temp_combo)
     else:
         # fam 2 is a list
@@ -119,7 +143,7 @@ def build_drills_combination(families, fam1, fam2, parameter):
             # iterate over all previous combinations that were found
             for drills_combo in fam2:
                 temp_combo = [stat for stat in drills_combo]
-                drill1_val = fam1_dict[drill1]['parameters'][parameter]["team's mean"]
+                drill1_val = fam1_dict[drill1]["parameters"][parameter]["team's mean"]
                 # add the new drill to the drill combination
                 temp_combo.insert(0, (drill1, drill1_val))
                 # update the combination's total sum of distance
@@ -129,12 +153,12 @@ def build_drills_combination(families, fam1, fam2, parameter):
 
 
 def build_session(sb, families_file="../data/families.json"):
-    '''
+    """
     builds a training session by a distance value training and specific families of drills
     :param families_file: frills families file path
     :param sb: sessions build params as a SessionBuild class
     :return: dictionary of training session options - total sum of the session and of each drill
-    '''
+    """
     families = open_json_file(families_file)
     drill_fams, parameter, low_val, high_val = sb.get_session_params()
     combos = list()
@@ -143,10 +167,17 @@ def build_session(sb, families_file="../data/families.json"):
     for drill_index in range(len(drill_fams) - 1):
         # if first iteration - the func should get two strings as an input
         if drill_index == 0:
-            combos = build_drills_combination(families, drill_fams[drill_index], drill_fams[drill_index + 1], parameter)
+            combos = build_drills_combination(
+                families,
+                drill_fams[drill_index],
+                drill_fams[drill_index + 1],
+                parameter,
+            )
         # we used the second drill in the first iteration
         elif drill_index != 1:
-            combos = build_drills_combination(families, drill_fams[drill_index], combos, parameter)
+            combos = build_drills_combination(
+                families, drill_fams[drill_index], combos, parameter
+            )
     # sort all combinations by the total distance value - from the highest to lowest
     combos.sort(key=lambda combo_lam: combo_lam[-1], reverse=True)
     # get the session's last family data
@@ -161,7 +192,7 @@ def build_session(sb, families_file="../data/families.json"):
             drill_val = 0
         else:
             find_parameter(parameter, last_fam[drill])
-            drill_val = last_fam[drill]['parameters'][parameter]["team's mean"]
+            drill_val = last_fam[drill]["parameters"][parameter]["team's mean"]
         # iterate over each combination that was found
         for combo in combos:
             # add the last family drill val to the combination's total sum
@@ -182,31 +213,44 @@ def build_session(sb, families_file="../data/families.json"):
                 option_num += 1
                 session_options[session_title] = {"drills": {}}
                 if len(drill_fams) != 2:
-                    session_options[session_title]["drills"][drill] = families[extract_family_name(drill)].get(drill)
-                    total_variance = families[extract_family_name(drill)][drill]['parameters'][parameter].get("team's variance")
+                    session_options[session_title]["drills"][drill] = families[
+                        extract_family_name(drill)
+                    ].get(drill)
+                    total_variance = families[extract_family_name(drill)][drill][
+                        "parameters"
+                    ][parameter].get("team's variance")
                 else:
                     total_variance = 0
                 for dr in combo:
                     if not tuple == type(dr):
                         break
-                    session_options[session_title]["drills"][dr[0]] = families[extract_family_name(dr[0])].get(dr[0])
-                    total_variance += families[extract_family_name(dr[0])][dr[0]]['parameters'][parameter].get("team's variance")
+                    session_options[session_title]["drills"][dr[0]] = families[
+                        extract_family_name(dr[0])
+                    ].get(dr[0])
+                    total_variance += families[extract_family_name(dr[0])][dr[0]][
+                        "parameters"
+                    ][parameter].get("team's variance")
                 session_options[session_title]["total mean"] = round(total_mean, 3)
-                session_options[session_title]["total variance"] = round(total_variance, 3)
+                session_options[session_title]["total variance"] = round(
+                    total_variance, 3
+                )
                 # calculate low dan high val probabilities
                 mean = session_options[session_title]["total mean"]
                 variance = session_options[session_title]["total variance"]
-                standard_deviation = variance ** 0.5
+                standard_deviation = variance**0.5
                 # Calculate the z-score for the boundary values
                 z_low = (low_val - mean) / standard_deviation
                 z_high = (high_val - mean) / standard_deviation
                 # CDF to calculate the probability of exceeding top and bottom boundaries
                 proba_low = norm.cdf(z_low)
                 proba_high = 1 - norm.cdf(z_high)
-                session_options[session_title]["exceeded limit probabilities"] = (round(proba_low, 3),
-                                                                                  round(proba_high, 3))
+                session_options[session_title]["exceeded limit probabilities"] = (
+                    round(proba_low, 3),
+                    round(proba_high, 3),
+                )
                 if not sb.check_constrains(session_options[session_title]["drills"]):
                     # constrains did not meet with the training session option
                     del session_options[session_title]
                     option_num -= 1
+    pprint.pprint(session_options)
     return session_options
--- app_main/helper_functions.py
+++ app_main/helper_functions.py
@@ -3,21 +3,21 @@ import re
 
 
 def open_json_file(path):
-    '''
+    """
     :param path: path to json file
     :return: file's data as dictionary
-    '''
+    """
     with open(path) as js_file:
         jf = json.load(js_file)
     return jf
 
 
 def write_to_json_file(json_file, data_dict):
-    '''
+    """
     :param json_file: path to json file
     :param data_dict: dict to write to the json file
     :return: writes data to json file, void func
-    '''
+    """
     out_file = open(json_file, "w")
     json.dump(data_dict, out_file, indent="")
     out_file.close()
@@ -26,44 +26,46 @@ def write_to_json_file(json_file, data_dict):
 
 def find_parameter(parameter, drill_dict):
     if parameter not in drill_dict["parameters"]:
-        raise ValueError(parameter, "wasn't found in the drill's dict, please check your spelling.")
+        raise ValueError(
+            parameter, "wasn't found in the drill's dict, please check your spelling."
+        )
 
 
 def extract_family_name(drill_name):
-    '''
+    """
     :param drill_name: drill's name as a string
     :return: family name
-    '''
+    """
     if drill_name[0] == " ":
         drill_name = drill_name[1:]
     try:
-        first_word = drill_name[:drill_name.index(" ")].lower()
+        first_word = drill_name[: drill_name.index(" ")].lower()
     except:
         first_word = drill_name.lower()
-    if 'ball' in first_word:
+    if "ball" in first_word:
         return "possession"
     elif "large" in first_word:
         return "lsg"
-    elif "vs" in first_word and '+' in first_word:
-        return 'joker games'
-    elif 'warm' in first_word or 'intervals' in first_word:
+    elif "vs" in first_word and "+" in first_word:
+        return "joker games"
+    elif "warm" in first_word or "intervals" in first_word:
         return "fitness"
-    elif 'sided' in first_word:
+    elif "sided" in first_word:
         return "sided games"
     else:
-        return first_word.lower().strip(',').replace(":", "")
+        return first_word.lower().strip(",").replace(":", "")
 
 
 def remove_set_num_from_drill_name(drill):
-    '''
+    """
     cleans csv add-ons to the column's name
     :param drill: drill's name
     :return: drill's name without the number of set
-    '''
-    if 'half' in drill:
+    """
+    if "half" in drill:
         return drill
     # find the '.' before the number of the occurrence and return the original name
-    substrings = drill.split(',')
+    substrings = drill.split(",")
     result = drill.lstrip().rstrip()
     if "set" in substrings[-1].lower():
         result = substrings[0]
@@ -71,16 +73,16 @@ def remove_set_num_from_drill_name(drill):
         for sub in substrings:
             if sub == result:
                 continue
-            result = result + ',' + sub
+            result = result + "," + sub
     result = result.lower()
 
-    if 'set' in result:
-        result = result[:result.index(' set')]
-    result = re.sub(r'(\s*\d+th$)', '', result)
-    result = re.sub(r'(\s*\d+nd$)', '', result)
-    result = re.sub(r'(\s*\d+st$)', '', result)
-    result = re.sub(r'(\s*\d+rd$)', '', result)
-    result = re.sub(r'(\s*\d+th$)', '', result)
-    result = re.sub(r'(\s*\d+nd$)', '', result)
-    result = re.sub(r'(\s*\d$)', '', result)
+    if "set" in result:
+        result = result[: result.index(" set")]
+    result = re.sub(r"(\s*\d+th$)", "", result)
+    result = re.sub(r"(\s*\d+nd$)", "", result)
+    result = re.sub(r"(\s*\d+st$)", "", result)
+    result = re.sub(r"(\s*\d+rd$)", "", result)
+    result = re.sub(r"(\s*\d+th$)", "", result)
+    result = re.sub(r"(\s*\d+nd$)", "", result)
+    result = re.sub(r"(\s*\d$)", "", result)
     return result
