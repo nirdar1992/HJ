125a47db741748d0d7458026f9398889ca5c08e6
nirdar1992 <131364971+nirdar1992@users.noreply.github.com>
2023-09-09 11:58:35 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,21 +2,146 @@ import unittest
 import os
 import json
 import tempfile
+import csv
 from app_main.build_training_session import *
 from app_main.helper_functions import open_json_file, write_to_json_file, find_parameter, extract_family_name, remove_set_num_from_drill_name
+from app_main.project_classes import SessionBuild
+from app_main.new_data_update import update_data
 
 
-class TestYourFunctions(unittest.TestCase):
+class TestUpdateData(unittest.TestCase):
     def setUp(self):
-        # create a temporary directory to store test json file
         self.temp_dir = tempfile.mkdtemp()
+        self.ref_families_file = os.path.join(self.temp_dir, "families.json")
+        self.ref_stats_file = os.path.join(self.temp_dir, "stats.json")
         
+        ref_families_data = {"family1": {}, "family2": {}}
+        ref_stats_data = {"drill1": {"parameters": {}}}
+        
+        with open(self.ref_families_file, "w") as ref_families_file:
+            json.dump(ref_families_data, ref_families_file)
+        
+        with open(self.ref_stats_file, "w") as ref_stats_file:
+            json.dump(ref_stats_data, ref_stats_file)
+        
+        self.ref_csv_file = os.path.join(self.temp_dir, "ref.csv")
+        with open(self.ref_csv_file, "w", newline="", encoding="utf-8") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            header = ["Session Number", "Drill Title", "Player Last Name", "Total Distance"]
+            csvwriter.writerow(header)
+            data_row = ["1", "Drill 1 Set 1", "Player1", "100"]
+            csvwriter.writerow(data_row)
+
+    def tearDown(self):
+        os.rmdir(self.temp_dir)
+
+    def test_update_data(self):
+        result = update_data(self.ref_csv_file, self.ref_families_file, self.ref_stats_file)
+        self.assertEqual(result, 0)
+        
+        with open(self.ref_stats_file, "r") as stats_file:
+            updated_stats_data = json.load(stats_file)
+        
+        with open(self.ref_families_file, "r") as families_file:
+            updated_families_data = json.load(families_file)
+            
+        self.assertIn("Drill 1 Set 1", updated_stats_data)
+        self.assertIn("Drill 1 Set 1", updated_families_data.get("family1", {}))
+
+
+class TestSessionBuildClass(unittest.TestCase):
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp()
+        self.ref_families_file = os.path.join(self.temp_dir, "families.json")
+        with open(self.ref_families_file, "w") as ref_families_file:
+            json.dump({"family1": "data1", "family2": "data2"}, ref_families_file)
+
+        self.ref_session_file = os.path.join(self.temp_dir, "BuildSession.json")
+        ref_session_data = {
+            "session": {
+                "fams": ["family1", "family2"],
+                "parameter": "Total Distance",
+                "min_val": 0,
+                "max_val": 100,
+            },
+            "constrains": [
+                {
+                    "fams": ["family1"],
+                    "operator": "range",
+                    "parameter": "Total Distance",
+                    "min_val": 10,
+                    "max_val": 50,
+                },
+                {
+                    "fams": ["family2"],
+                    "operator": "sum range",
+                    "parameter": "Total Distance",
+                    "min_val": 30,
+                    "max_val": 80,
+                },
+            ],
+        }
+        with open(self.ref_session_file, "w") as ref_session_file:
+            json.dump(ref_session_data, ref_session_file)
+
+    def tearDown(self):
+        os.rmdir(self.temp_dir)
+
+    def test_session_build_init(self):
+        session_build = SessionBuild(session_dict_path=self.ref_session_file)        
+        self.assertEqual(session_build.session_fams, ["family1", "family2"])
+        self.assertEqual(session_build.parameter, "Total Distance")
+        self.assertEqual(session_build.min_val, 0)
+        self.assertEqual(session_build.max_val, 100)
+
+    def test_check_session_params(self):
+        invalid_session_data = {
+            "session": {
+                "fams": ["invalid_family"],
+                "parameter": "Invalid Parameter",
+                "min_val": -10,
+                "max_val": 5,
+            }
+        }
+        invalid_session_path = os.path.join(self.temp_dir, "InvalidSession.json")
+        with open(invalid_session_path, "w") as invalid_session_file:
+            json.dump(invalid_session_data, invalid_session_file)
+
+        with self.assertRaises(ValueError):
+            SessionBuild(session_dict_path=invalid_session_path)
+
+    def test_check_constrain_params(self):
+        invalid_constraints_data = {
+            "session": {
+                "fams": ["family1", "family2"],
+                "parameter": "Total Distance",
+                "min_val": 0,
+                "max_val": 100,
+            },
+            "constrains": [
+                {
+                    "fams": "invalid_family",  # Invalid: should be a list
+                    "operator": "range",
+                    "parameter": "Total Distance",
+                    "min_val": 10,
+                    "max_val": 50,
+                }]}
+        
+        invalid_constraints_path = os.path.join(self.temp_dir, "InvalidConstraints.json")
+        with open(invalid_constraints_path, "w") as invalid_constraints_file:
+            json.dump(invalid_constraints_data, invalid_constraints_file)
+        with self.assertRaises(ValueError):
+            SessionBuild(session_dict_path=invalid_constraints_path)
+
+
+class TestHelperFunctions(unittest.TestCase):
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp()
         self.reference_json_path = os.path.join(self.temp_dir, "reference.json")
         with open(self.reference_json_path, "w") as reference_json_file:
             json.dump({"test": "test"}, reference_json_file)
             
     def tearDown(self):
-        # remove the temporary directory and its contents.
         os.rmdir(self.temp_dir)
 
     def test_open_json_file(self):
